14365d966756b126d6a1886f1e6e7e38886f8f12
lorenzo800 <previtali.lorenzo.studente@itispaleocapa.it>
2026-02-20 10:24:11 +0100

    Add files via upload
    Struttura

---
+++ Previtali.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+	//struct porodotto con nome ,categoria e prezzo
+	struct prodotto{           
+	    string nome;
+		string categoria;
+		float prezzo;	
+	}
+	
+	
+int main(){
+	
+	
+	
+	
+	
+	
+	return 0;
+}
