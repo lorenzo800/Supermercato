06751c9db0f8321afafe59b6332cd20cf94702af
lorenzo800 <previtali.lorenzo.studente@itispaleocapa.it>
2026-02-20 10:32:48 +0100

    Add files via upload
    Funzioni

--- Previtali.cpp
+++ Previtali.cpp
@@ -6,13 +6,37 @@ using namespace std;
 	    string nome;
 		string categoria;
 		float prezzo;	
+	};
+	
+	
+	void aggiungi(prodotto p, int d, prodotto p[]){
+		
+		
+	}
+	
+	
+	void visualizza(prodotto p[], int d){
+		
+	}
+	
+	
+	void cerca(prodotto p,prodotto p[] , int d){
+		
+	}
+	
+	bool cancella(prodotto p,prodotto p[] , int d){
+		
+	}
+	
+	bool modifica(prodotto p,prodotto p[] , int d){
+		
 	}
 	
 	
 int main(){
 	
-	
-	
+	prodotto p;
+	prodotto supermercato[100];
 	
 	
 	
