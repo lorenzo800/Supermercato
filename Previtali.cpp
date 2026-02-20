195b7e364fee97102505341fb19b9e62b94224ef
lorenzo800 <previtali.lorenzo.studente@itispaleocapa.it>
2026-02-20 10:51:48 +0100

    Add files via upload
    menù

--- Previtali.cpp
+++ Previtali.cpp
@@ -25,11 +25,11 @@ using namespace std;
 	}
 	
 	bool cancella(prodotto p,prodotto p[] , int d){
-		
+	return true;	
 	}
 	
 	bool modifica(prodotto p,prodotto p[] , int d){
-		
+		return true;
 	}
 	
 	
@@ -38,7 +38,38 @@ int main(){
 	prodotto p;
 	prodotto supermercato[100];
 	
+	int s;
+	
+	
+	
+	
+	
+	do{
+	cout<<"inserisci il valore di s(1= aggiungi,2=vizualizza,3=cerca,4=cancella,5=modifica)"
+	cin>>s;
+	
+	switch case(s){
+	case 1:
+	aggiungi(prodotto p, int d, prodotto p[]);
+	break;
+		case 2:
+	visualizza(prodotto p[], int d);
+	break;
+		case 3:
+	cerca(prodotto p,prodotto p[] , int d);
+	break;
+		case 4:
+	cancella(prodotto p,prodotto p[] , int d);
+	break;
+		case 5:
+	modifica(prodotto p,prodotto p[] , int d);
+	break;	
+	}while(s!=0);
+	
+	
+}
 	
 	
 	return 0;
+
 }
